7b813b1323f8c6eccafe3bab966275f2915b6c5a
Somnath Pichewar <somnathpichewar@gmail.com>
2025-10-01 13:19:54 +0530

    OOPs completed

--- Python.py
+++ Python.py
@@ -1,5 +1,5 @@
-
-
+#BASICS ARE COMPLETED
+#OOPS CONCEPTS IN PYTHON
 
 #CLASS
 class Car:
@@ -12,25 +12,32 @@ class Car:
 
     def full_name(self):
         return f"{self.__brand} {self.model}"
-
+    
+    def fuel_type(self):     #POLYMORPHISM 
+        return "Petrol or Diesel"
+    def genereal(self):
+        return "Cars are means of transport"
 
 #INHERITENCE
 class Electric_car(Car):
     def __init__(self,brand,model, battery_size):
         super().__init__(brand, model)
         self.battery_size = battery_size
-
+    def fuel_type(self):    #POLYMORPHISM
+        return "Battery recharge"
 
 
 
 
 my_tesla = Electric_car("Tesla","Model S", "85KWH")
-print(my_tesla.get_brand())
+# print(my_tesla.fuel_type())
+
+
+simple_car = Car("Tata", "Safari")
+# print(simple_car.fuel_type())
 
 
 
-
-
-my_car = Car("Compass","Top_End")
+print(simple_car.genereal())
 # print(my_car.brand)
 # print(my_car.full_name())
